22bd91cba2ff741a05b4af4c2713e50336ce4bd2
Karim Chaouki <kchaouki@c2r7p6.1337.ma>
2023-04-17 18:46:41 +0000

    cpp

---
+++ test.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+// using namespace std;
+// int main()
+// {
+// 	// stdout
+// 	cout << "test from C++";
+	
+// 	// stdin
+// 	// cin
+
+// 	// stderr
+// 	// cerr
+
+// 	// general information
+// 	// clog
+// 	return (0);
+// }
+
+/*--------------------------------*/
+
+// int main()
+// {
+// 	std::cout << "Enter two numbers:" << std::endl;
+// 	int n1 = 0, n2 = 0;
+// 	std::cin >> n1 >> n2;
+// 	std::cout << "The sum of " << n1 << " and " << n2 << " is " << n1 + n2 << std::endl;
+// 	return (0);
+// }
+
+/*-----------NAMESPACE------------*/
+// using namespace std;
+
+// int main()
+// {
+// 	cout << "Enter two numbers:" << endl;
+// 	int n1 = 0, n2 = 0;
+// 	cin >> n1 >> n2;
+// 	cout << "The sum of " << n1 << " and " << n2 << " is " << n1 + n2 << endl;
+// 	return (0);
+// }
+
+/*-----------CLASSES------------*/
+
+// class Book
+// {
+// 	public:
+// 		std::string	title;
+// 		std::string	author;
+// 		int			pages;
+
+// };
+
+// int main()
+// {
+// 	Book book;
+// 	book.title = "to heaven";
+// 	book.author = "karim chaouki";
+// 	book.pages = 120;
+// 	std::cout << book.author << " " << book.title << " " << book.pages;
+// 	return (0);
+// }
+
+/*-----------CONSTRUCTOR FUNCTION------------*/
+
