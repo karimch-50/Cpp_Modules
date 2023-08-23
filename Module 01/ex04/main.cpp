18e223e1565e58085b60637fcff16e5bdee65832
karimch-50 <karimchaouki50@gmail.com>
2023-08-23 17:26:26 +0100

    ex04

---
+++ Module 01/ex04/main.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchaouki < kchaouki@student.1337.ma>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/23 14:47:44 by kchaouki          #+#    #+#             */
+/*   Updated: 2023/08/23 17:20:53 by kchaouki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main(int ac, char **av)
+{
+	if (ac == 4)
+	{
+		std::string fileName = av[1];
+		std::string toReplace = av[2];
+		std::string replaceBy = av[3];
+		std::ifstream infile(fileName.c_str());
+		if (!infile)
+		{
+			std::cout << "failed to open the file!!" << std::endl;
+			return (1);
+		}
+		std::ofstream outfile((fileName + ".replace").c_str());
+		if (!infile)
+		{
+			std::cout << "failed to create the file!!" << std::endl;
+			return (1);
+		}
+		std::string	buffer;
+		std::string line;
+		size_t		index;
+		while (std::getline(infile, line))
+		{
+			buffer = line;
+			while (buffer.length() > 0)
+			{
+				index = buffer.find(toReplace);
+				outfile << buffer.substr(0, index);
+				if (index < buffer.length())
+				{
+					outfile << replaceBy;
+					buffer = buffer.substr(index + toReplace.length(), buffer.length());
+				}
+				else
+					break ;
+			}
+			outfile << std::endl;
+		}
+		infile.close();
+		outfile.close();
+	}
+	return (0);
+}
