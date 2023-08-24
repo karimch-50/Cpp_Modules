453e4cac665163d71c5fd952b23770b5261cd7d8
karimch-50 <karimchaouki50@gmail.com>
2023-08-24 13:05:33 +0100

    ex06

---
+++ Module 01/ex06/Harl.cpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchaouki < kchaouki@student.1337.ma>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/23 21:52:38 by kchaouki          #+#    #+#             */
+/*   Updated: 2023/08/24 13:04:29 by kchaouki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+void	Harl::debug(void)
+{
+	std::cout << "[ DEBUG ]\n";
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger."; 
+	std::cout << " I really do!" << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "[ INFO ]\n";
+	std::cout << "I cannot believe adding extra bacon costs more money.";
+	std::cout << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+void	Harl::warning(void)
+{	
+	std::cout << "[ WARNING ]\n";
+	std::cout << "I think I deserve to have some extra bacon for free.";
+	std::cout << "I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "[ ERROR ]\n";
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;	
+}
+
+void	Harl::complain(std::string level)
+{
+	std::string  levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int lvl = 0;
+	while (lvl < 4 && level != levels[lvl])
+		lvl++;
+	switch (lvl)
+	{
+		case 0:
+			Harl::debug();
+		case 1:
+			Harl::info();
+		case 2:
+			Harl::warning();
+		case 3:
+		{
+			Harl::error();
+			break ;
+		}
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;	
+	}
+}
---
+++ Module 01/ex06/Harl.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchaouki < kchaouki@student.1337.ma>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/23 21:52:36 by kchaouki          #+#    #+#             */
+/*   Updated: 2023/08/24 12:45:35 by kchaouki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+
+class Harl
+{
+	private:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+	public:
+		void complain(std::string level);
+};
+
+#endif
