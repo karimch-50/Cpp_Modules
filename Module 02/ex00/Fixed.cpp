7152216a2651c0b2409524dcaa18f9be8ed780e1
karimch-50 <karimchaouki50@gmail.com>
2023-08-27 07:36:26 +0100

    handle one small error

--- Module 02/ex00/Fixed.cpp
+++ Module 02/ex00/Fixed.cpp
@@ -6,13 +6,13 @@
 /*   By: kchaouki < kchaouki@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 11:31:06 by kchaouki          #+#    #+#             */
-/*   Updated: 2023/08/26 12:39:25 by kchaouki         ###   ########.fr       */
+/*   Updated: 2023/08/26 18:28:41 by kchaouki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-const int	f_bits = 8;
+const int	Fixed::f_bits = 8;
 
 Fixed::Fixed()
 {
