68b0b34e8dd3230e2b83dd04e86f350d25686cf2
Karim Chaouki <kchaouki@c1r3p9.1337.ma>
2023-09-02 10:13:31 +0100

    test

--- test.cpp
+++ test.cpp
@@ -328,6 +328,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <cmath>
 using namespace std;
 
 // int main () {
@@ -356,17 +357,58 @@ using namespace std;
 //     return (0);
 // }
 
+// int main()
+// {
+//     int day = 7;
+//     switch (day) {
+//     case 6:
+//         cout << "Today is Saturday";
+//         break;
+//     case 7:
+//         cout << "Today is Sunday";
+//         break;
+//     default:
+//         cout << "Looking forward to the Weekend";
+//     }
+// }
+
+
+float area(int x1, int y1, int x2, int y2, int x3, int y3)
+{
+   return abs((x1*(y2-y3) + x2*(y3-y1)+ x3*(y1-y2))/2.0);
+}
+  
+/* A function to check whether point P(x, y) lies inside the triangle formed
+   by A(x1, y1), B(x2, y2) and C(x3, y3) */
+bool isInside(int x1, int y1, int x2, int y2, int x3, int y3, int x, int y)
+{  
+   /* Calculate area of triangle ABC */
+   float A = area (x1, y1, x2, y2, x3, y3);
+  
+   /* Calculate area of triangle PBC */ 
+   float A1 = area (x, y, x2, y2, x3, y3);
+  
+   /* Calculate area of triangle PAC */ 
+   float A2 = area (x1, y1, x, y, x3, y3);
+  
+   /* Calculate area of triangle PAB */  
+   float A3 = area (x1, y1, x2, y2, x, y);
+    
+   /* Check if sum of A1, A2 and A3 is same as A */
+   return (A == A1 + A2 + A3);
+}
+  
+/* Driver program to test above function */
 int main()
 {
-    int day = 7;
-    switch (day) {
-    case 6:
-        cout << "Today is Saturday";
-        break;
-    case 7:
-        cout << "Today is Sunday";
-        break;
-    default:
-        cout << "Looking forward to the Weekend";
-    }
+   /* Let us check whether the point P(10, 15) lies inside the triangle
+      formed by A(0, 0), B(20, 0) and C(10, 30) */
+   if (isInside(1, 1, 3, 4, 4, 1, 3, 2))
+     cout <<"Inside";
+   else
+     cout <<"Not Inside";
+  
+   return 0;
 }
+
+ 
